ff132ab5f2289acbfc69945977b54aa67e049a7e
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-01 14:47:01 +0530

    ..

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
-import routes from "./routess";
+import routes from "./routes";
 import  deserializeUser from "./middleware/deserializeUser";
 
 const app = express();
